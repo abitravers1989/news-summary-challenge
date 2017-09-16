d41c98880ec3a599895ffd26221dfb58873f6b17
Abi Travers <atravers4007@gmail.com>
2017-09-16 12:09:14 +0100

    Learning JSON

---
+++ app.js
@@ -0,0 +1,23 @@
+var newsStory1 = {
+  "headline": "Evil Fire kills 3 ratpigeons",
+  "summary": "More bad things are happening and they have no relation to your life but you should take this as a warning to behave.",
+  "category": "Bad things in UK"
+}
+
+newsStory1.category
+var myNewsStories = ["fire", "treehands", "11buttons"]
+
+myNewsStories[2]
+
+var  theBadNewsStories = [
+  {
+    "headline": "Evil Fire kills 3 ratpigeons",
+    "summary": "More bad things are happening and they have no relation to your life but you should take this as a warning to behave.",
+    "category": "Bad things in UK"
+  },
+  {
+    "headline": "Ratpigeons Attack",
+    "summary": "The Ratpigeons will eventually get to you. You must spend time building your tinfoil home and live there to survive.",
+    "category": "Bad things in World"
+  }
+]
