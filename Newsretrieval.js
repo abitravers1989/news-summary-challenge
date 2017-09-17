ec1ac898c00e02d1014efdfc830c6315c6802245
Abi Travers <atravers4007@gmail.com>
2017-09-17 09:16:32 +0100

    Changing setup to constructor prototype

---
+++ Newsretrieval.js
@@ -0,0 +1,59 @@
+
+(function(exports){
+  function Newsretrieval() {
+     var originalHttpReq = this;
+     console.log(originalHttpReq)
+     var data;
+     console.log(data)
+     var dataReq;
+  };
+
+  Newsretrieval.prototype.get = function(){
+
+    var ourRequest = new XMLHttpRequest();
+    ourRequest.open("GET", "https://content.guardianapis.com/search?q=debates&api-key=test");
+    console.log(ourRequest);
+    ourRequest.onload = function (){
+      data = JSON.parse(ourRequest.responseText);
+      console.log(data)
+      dataReq = data.results;
+      console.log(dataReq)
+    };
+   ourRequest.send();
+  };
+
+  exports.Newsretrieval = Newsretrieval;
+})(this);
+
+
+// function setup(){
+//
+//   loadJSON("jsonfile.json", gotData);
+//
+// }
+//
+// function gotData(data) {
+//   printLn(data);
+// }
+
+
+
+
+
+
+// var newsSummary = document.getElementById("news-summary");
+//
+// function rednerHTML(data){
+//   var htmlString = " ";
+//
+//   for (i = 0; i < data.length; i++){
+//     var element = data[i]
+//     console.log(element)
+//     var displayList = "<p>" + data[i] + "</p>"
+//     console.log(displayList)
+//   }
+//   newsSummary.insertAdjacentHTML('beforeend', htmlString);
+//   console.log(element)
+//   console.log(newsSummary)
+// }
+//
